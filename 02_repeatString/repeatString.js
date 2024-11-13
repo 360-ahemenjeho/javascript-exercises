579502dec83c09b1e33cd834e6ba4e01eba9647a
Aondoahemen Abraham <159141014+360-ahemenjeho@users.noreply.github.com>
2024-11-13 22:27:34 +0100

    chore: update

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,10 @@
-const repeatString = function() {
-
-};
+const repeatString = function (str, n) {
+  let temp = ''
+  for (let i = 0; i < n; i++) {
+    temp += str
+  }
+  return temp
+}
 
 // Do not edit below this line
-module.exports = repeatString;
+module.exports = repeatString
