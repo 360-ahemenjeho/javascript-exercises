6489a9b060a7c117d3e837abc9bf5db995af3f4b
Aondoahemen Abraham <159141014+360-ahemenjeho@users.noreply.github.com>
2024-11-18 22:11:46 +0100

    chore: update

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,12 @@
-const removeFromArray = function() {
-};
+const removeFromArray = function (arr, ...searchEl) {
+  const newArr = []
+  arr.forEach((item) => {
+    if (!searchEl.includes(item)) newArr.push(item)
+  })
+  return newArr
+}
+
+removeFromArray([1, 2, 3, 4], 3)
 
 // Do not edit below this line
-module.exports = removeFromArray;
+module.exports = removeFromArray
