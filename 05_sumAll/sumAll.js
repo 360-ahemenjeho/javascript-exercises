bc6d6301cc2cd7edd8a8162f1db74b2766912cb4
Aondoahemen Abraham <159141014+360-ahemenjeho@users.noreply.github.com>
2024-11-19 09:34:41 +0100

    chore: update

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,19 @@
-const sumAll = function() {
+const sumAll = function (x, y) {
+  if (!Number.isInteger(x) || !Number.isInteger(y) || x < 1 || y < 1 || x === y)
+    return 'ERROR'
 
-};
+  const isGreaterX = x > y
+  let min = isGreaterX ? y : x
+  const max = isGreaterX ? x : y
+  let tmp = 0
+
+  while (min <= max) {
+    tmp = tmp + min
+    min++
+  }
+
+  return tmp
+}
 
 // Do not edit below this line
-module.exports = sumAll;
+module.exports = sumAll
